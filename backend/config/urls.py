64dcea451c9f4c9b4c542a92d5e3755b7306f0ad
dkotadiya018 <144389500+dkotadiya018@users.noreply.github.com>
2026-08-27 15:23:17 +0530

    Fix: Enable production media file serving in urls.py for product images

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from products.views import index
 from rest_framework.routers import DefaultRouter
 from products.api import ProductViewSet, CategoryViewSet
@@ -29,4 +29,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('calculator/', include('calculator.urls')),
     path('api/', include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
