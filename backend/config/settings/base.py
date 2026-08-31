f60818f15180f71c339f4b0114d5a1ae6bf99184
dkotadiya018 <144389500+dkotadiya018@users.noreply.github.com>
2026-08-31 18:09:40 +0530

    fix(admin): enable APP_DIRS in TEMPLATES to allow loading Django admin templates

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -106,8 +106,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates_archive'],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
+
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
